c68d358fa0f61ec26db52b31b87605b91b36eb7d
Sohailbasha <shlbsh94@gmail.com>
2017-08-19 17:24:25 -0400

    Creating custom files for custom UIElements

---
+++ ProtocolAnimating/DataEntryTextField.swift
@@ -0,0 +1,17 @@
+//
+//  DataEntryTextField.swift
+//  ProtocolAnimating
+//
+//  Created by Ilias Basha on 8/19/17.
+//  Copyright © 2017 Sohail. All rights reserved.
+//
+
+import UIKit
+
+class DataEntryTextField: UITextField {
+
+    
+    
+    
+
+}
---
+++ ProtocolAnimating/LogInButton.swift
@@ -0,0 +1,17 @@
+//
+//  LogInButton.swift
+//  ProtocolAnimating
+//
+//  Created by Ilias Basha on 8/19/17.
+//  Copyright © 2017 Sohail. All rights reserved.
+//
+
+import UIKit
+
+class LogInButton: UIButton {
+
+
+    
+    
+
+}
