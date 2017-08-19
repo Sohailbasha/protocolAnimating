6c92fbaae65a3eac47672fc7d919c73c21048c7c
Sohailbasha <shlbsh94@gmail.com>
2017-08-19 17:25:21 -0400

    added custom file for label

---
+++ ProtocolAnimating/FlashingLabel.swift
@@ -0,0 +1,21 @@
+//
+//  FlashingLabel.swift
+//  ProtocolAnimating
+//
+//  Created by Ilias Basha on 8/19/17.
+//  Copyright © 2017 Sohail. All rights reserved.
+//
+
+import UIKit
+
+class FlashingLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
